07b882e4aaa0d2365f06fa3eef8b8141903d6719
phongnd39 <nguyenphong.hanu@gmail.com>
2023-05-18 18:39:46 +0900

    :+1: Update endpoint matcher

--- src/ky-plugin.ts
+++ src/ky-plugin.ts
@@ -1,12 +1,18 @@
 import ky from 'ky-universal'
 import { AxiosError } from 'axios'
+import { match } from 'path-to-regexp'
 import { KyPluginOptions, KyZodiosPlugin} from '../index'
 
 export function KyPlugin (options?: KyPluginOptions): KyZodiosPlugin {
   return {
     name: 'ky',
     request: async (api, config) => {
-      const endpoint = api.find(endpoint => endpoint.path.replace(/^\//, '') === config.url.replace(/^\//, '') && endpoint.method === config.method)
+      const endpoint = api.find(endpoint => {
+        const urlMatchFn = match(endpoint.path)
+        const checkUrl = `/${config.url.replace(/^\//, '')}`
+
+        return urlMatchFn(checkUrl) && endpoint.method === config.method
+      })
 
       return {
         transformRequest: [(data: any) => data],
